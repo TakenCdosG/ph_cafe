d932ee436baec1ae9504e22e19bffcc39a3cf5b9
Maicol Lopez <maicol.lopez@akendos.com>
2015-04-28 14:22:49 -0500

    Footer Buttons Overlay

---
+++ wp-content/themes/wpex-elegant/assets/js/custom-scripts.js
@@ -0,0 +1,20 @@
+jQuery( function($) {
+
+    $(document).ready(function() {
+
+
+        var ClassToAdd = jQuery("#sidr-id-menu-main-menu  > li.sidr-class-current-menu-ancestor > a").text();
+
+        var ClassToAdd = ClassToAdd.toLowerCase();
+
+        jQuery("#footer-buttons a."+ClassToAdd+" img").css({
+            "-webkit-filter": "grayscale(0)",
+            "filter": "grayscale(0)"
+        });
+
+
+       // alert(ClassToAdd);
+
+    });
+
+});
