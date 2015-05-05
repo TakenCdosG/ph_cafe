0c0003ce05c532321b86b510544679d73f87b401
Maicol Lopez <maicol.lopez@akendos.com>
2015-05-05 16:40:30 -0500

    Events Calendar Updates

--- wp-content/plugins/the-events-calendar/views/default-template.php
+++ wp-content/plugins/the-events-calendar/views/default-template.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
 
- ?>
+get_header(); ?>
 	<div id="tribe-events-pg-template">
 		<?php tribe_events_before_html(); ?>
 		<?php tribe_get_view(); ?>
