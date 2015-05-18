d23cb976ac467dbf9a46c3f4f14d8c9d88ee39d8
Maicol Lopez <maicol.lopez@akendos.com>
2015-05-18 11:38:24 -0500

    Google Analytics

--- wp-content/themes/wpex-elegant/header.php
+++ wp-content/themes/wpex-elegant/header.php
@@ -25,6 +25,16 @@
         window._fbq = window._fbq || [];
         window._fbq.push(['track', 'PixelInitialized', {}]);
     </script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-37846218-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
     <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=420487224790880&amp;ev=PixelInitialized" /></noscript>
     <script src="//use.typekit.net/ryz6zwj.js"></script>
     <script>try{Typekit.load();}catch(e){}</script>
